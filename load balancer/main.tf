25f5f39d6c72d18a097fd9696e024df8b4b29dd1
biswajit12azure <biswajit.azuredevops@gmail.com>
2024-09-12 16:52:42 +0530

    recent changes

--- load balancer/main.tf
+++ load balancer/main.tf
@@ -1,6 +1,5 @@
 provider "azurerm" {
   features {}
-  subscription_id = "d87b0440-7342-4f1b-aa3a-83f0e1745c41"
 }
 
 resource "azurerm_resource_group" "example" {
--- using variables/main.tf
+++ using variables/main.tf
@@ -2,8 +2,6 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = "d87b0440-7342-4f1b-aa3a-83f0e1745c41"
-  tenant_id       = "e7330516-ceaa-4bbd-a5bb-6564744a4ce5"
 }
 
 # Define a resource
